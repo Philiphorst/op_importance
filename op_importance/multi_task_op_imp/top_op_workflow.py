603496c3b586470aaa6025a28fa15579493a04e2
philiphorst <philip.knaute@gmail.com>
2015-10-08 12:48:27 +0200

    add hierarchical clustering of correlated operations

--- op_importance/multi_task_op_imp/top_op_workflow.py
+++ op_importance/multi_task_op_imp/top_op_workflow.py
@@ -1,3 +1,18 @@
+'''
+Created on 1 Jul 2015
+
+@author: philip knaute
+------------------------------------------------------------------------------
+Copyright (C) 2015, Philip Knaute <philiphorst.project@gmail.com>,
+
+This work is licensed under the Creative Commons
+Attribution-NonCommercial-ShareAlike 4.0 International License. To view a copy of
+this license, visit http://creativecommons.org/licenses/by-nc-sa/4.0/ or send
+a letter to Creative Commons, 444 Castro Street, Suite 900, Mountain View,
+California, 94041, USA.
+------------------------------------------------------------------------------
+'''
+
 import glob 
 import numpy as np
 import cPickle as pickle
